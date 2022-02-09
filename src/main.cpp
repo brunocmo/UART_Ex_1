2a89d1ed835ab03f80c3076e409872910ca9ce62
Bruno Nunes <brunocmo.nunes@gmail.com>
2022-02-09 11:35:07 -0300

    Add more tests

--- src/main.cpp
+++ src/main.cpp
@@ -4,11 +4,21 @@ int main() {
 
     Comms ardu;
 
+    char inteiroTeste[] = { 0, 0 , 0 , 4 };
+
+    char realTeste[] = { 0, 0, 0, 3}; 
+
     ardu.pedidoInteiro();
 
     ardu.pedidoReal();
 
     ardu.pedidoString();
+
+    ardu.enviarInteiro(inteiroTeste);
+
+    ardu.enviarReal(realTeste);
+
+    ardu.enviarString("Bom dia!");
 
     return 0;
 }
